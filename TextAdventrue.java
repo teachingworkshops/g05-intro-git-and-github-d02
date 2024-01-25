991b2bd826bb8083cd18f9c291b069e1b09a11d2
budwayaATWIT <Budwaya@wit.edu>
2024-01-25 16:44:50 -0500

    Test commit

---
+++ TextAdventrue.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;/**
+ * TextAdventrue
+ */
+public class TextAdventrue {
+public static void main(String[] args) {
+
+    Scanner input= new Scanner(System.in);
+    System.out.println( "You have awoken in your bed on the ship");
+    
+}
+
+}
