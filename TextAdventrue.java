0249a54768beea0087a9d3414c0373320ee0e37d
Dustin Bloom <dw.is.life11@gmail.com>
2024-01-29 14:35:39 -0500

    Created an example of  user input

--- TextAdventrue.java
+++ TextAdventrue.java
@@ -2,11 +2,23 @@ import java.util.Scanner;/**
  * TextAdventrue
  */
 public class TextAdventrue {
+
+public static void test(){
+    System.out.println("Works");
+
+}
 public static void main(String[] args) {
 
-    Scanner input= new Scanner(System.in);
-    System.out.println( "You have awoken in your bed on the ship");
-    
+    Scanner userInput = new Scanner(System.in);
+    System.out.println( "Welcome to Space Donut!");
+
+    System.out.println( "Type Start to begin!");
+
+    //Example of User Input getting processed and calling a new function ("Room")
+    String choice = userInput.nextLine();
+    if(choice.contains("start")){
+        test();
+    };
 }
 
 }
