ee8e31c22b60ce4704261310c22b851f462c0351
Dustin Bloom <dw.is.life11@gmail.com>
2024-01-29 18:49:53 -0500

    Added TODOs for Each room, added decision tree for bedroom, added ending for back to bed

--- Rooms.java
+++ Rooms.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Rooms {
     
  /*   Bedroom 1 (The Starting Room)
@@ -5,7 +7,71 @@ public class Rooms {
   *   ENDING: Go-Back-To-Bed
   *   EXITS: Bathroom1 & Hall1
  */
-    public static void Bedroom1(){
-       System.out.println("Works"); 
+    public static void Bedroom1() throws InterruptedException{
+        Scanner userInput = new Scanner(System.in);
+       System.out.println("LOCATION: Your Bedroom");
+       Thread.sleep(1500);
+       System.out.println("> In your bedroom, you look at your surroundings");
+       System.out.println("> You notice a few things: A door to the hall, A door to the bathroom, Your bed and a dresser");
+       System.out.println("SELECT: Hall, Bathroom, Bed, Dresser");
+
+       String choice = userInput.nextLine();
+
+       
+        switch (choice) {
+            case "hall": //Player chooses the hall
+                hall1();
+                
+                break;       
+            case "bathroom" : //Player chooses the bathroom
+                bath1();
+            
+                break;
+            case "bed" : //User chooses the bed
+                Endings.b2b();
+                break;
+            case "dresser":
+                System.out.println("\n Inside the dresser contains clothing that you are already wearing ");
+                Thread.sleep(2000);
+                Bedroom1(); //Loop back
+                break;
+            default:
+                System.out.println("Please make a valid choice (Case Sensitive, all lowercase)");
+                Bedroom1();
+                break;
+           }
+           userInput.close();
+              
+
+        
     }
+    public static void hall1(){//TODO: Finish (Dustin)
+        System.out.println("Hall Test");
+    }
+    //Bathroom 1 (Connecte to starting room)
+    public static void bath1(){//TODO: Finish (Dustin)
+        System.out.println("Bath Test");
+    }
+
+    public static void airlock(){//TODO: Finish (Dustin)
+        System.out.println("Airlock Test");
+    }
+
+
+
+    //TODO:  BED/BR2
+    //TODO:  BED/BR3
+    //TODO:  Dining/Kitchen
+
+    //TODO:  Hall2
+    //TODO: Comms Room
+    //TODO: Armory
+    //TODO: Pilots room
+
+
+    //TODO: Hall3
+    //TODO: Engine Roon
+    //TODO: Boiler Room
+    
+
 }
