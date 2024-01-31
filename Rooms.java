38e16a69c0e521a37fe4460cdfca597f246b48eb
Dustin Bloom <dw.is.life11@gmail.com>
2024-01-31 01:07:15 -0500

    Nearly completed Hall1, Airlock, Bath1. Waiting on other features. #17

--- Rooms.java
+++ Rooms.java
@@ -11,8 +11,7 @@ public class Rooms {
         Scanner userInput = new Scanner(System.in);
        System.out.println("LOCATION: Your Bedroom");
        Thread.sleep(1500);
-       System.out.println("> In your bedroom, you look at your surroundings");
-       System.out.println("> You notice a few things: A door to the hall, A door to the bathroom, Your bed and a dresser");
+       System.out.println("> In your bedroom, you look at your surroundings \n> You notice a few things: A door to the hall, A door to the bathroom, Your bed and a dresser");
        System.out.println("SELECT: Hall, Bathroom, Bed, Dresser");
 
        String choice = userInput.nextLine();
@@ -20,17 +19,17 @@ public class Rooms {
        
         switch (choice) {
             case "hall": //Player chooses the hall
-                hall1();
+                Hall1();
                 break;       
             case "bathroom" : //Player chooses the bathroom
-                bath1();
+                Bath1();
                 break;
             case "bed" : //User chooses the bed
                 Endings.b2b();
                 break;
             case "dresser":
                 System.out.println("\n Inside the dresser contains clothing that you are already wearing ");
-                Thread.sleep(2000);
+                Thread.sleep(1500);
                 Bedroom1(); //Loop back
                 break;
             default: 
@@ -42,28 +41,134 @@ public class Rooms {
               
     }
 
-    //Bathroom 1 (Connected to starting Bathoom                                                                                                                                                                           room)
-    public static void bath1(){//TODO: Finish (Dustin)
+    /* Bathroom 1
+     *   This room contains 1 exits
+     *   EXITS: Bedroom1
+    */                                                                                                                                                                    
+    public static void Bath1() throws InterruptedException{
         Scanner userInput = new Scanner(System.in);
         
-        System.out.println("LOCATION: Your Bathroom");
+        System.out.println("\nLOCATION: Your Bathroom");
         Thread.sleep(1500);
+        System.out.println("> 'I don't really need to use the bathroom right now, I am pretty cleaned out.'");
+        Thread.sleep(1500);
+        System.out.println("> In the bathroom you see: A mirror and a weird key sitting upon the tank of the toilet");
+        System.out.println("SELECT: Mirror, Key, Bedroom");
+
+        String choice = userInput.nextLine();
+        switch(choice){
+            case "mirror":
+                System.out.println("> You look in the mirror and see yourself, hair is a mess, some lines on your cheek from sleeping on your arms\n> 'What the hell happened last night?'");
+                Thread.sleep(1500);
+                Bath1();
+                break;
+            case "key":
+                //TODO: After inventory tracking is implemented, add following code
+                /*
+                 * if(key = false)
+                 *       System.out.println("You pick up the bronze key, unsure as to what it opens");
+                 *       key = true
+                 * else
+                 *      System.out.println("You already picked up the key");
+                 */
+                System.out.println("> You pick up the bronze key, unsure as to what it opens");
+                Thread.sleep(1500);
+                Bath1();
+                break;
+            case "bedroom":
+                Bedroom1();
+                break;
+            default:
+            System.out.println("Please make a valid choice (Case Sensitive, all lowercase)");
+            Bath1();
+            break;
+        }
 
         userInput.close();
     }
 
-    public static void airlock(){//TODO: Finish (Dustin)
+    /* Airlock 
+     *   This room contains 2 ending, and 1 exit
+     *   ENDINGS: Space Donut & Deadly Donut
+     *   EXITS: Hall 1
+    */
+    public static void Airlock() throws InterruptedException{
         Scanner userInput = new Scanner(System.in);
         
         System.out.println("LOCATION: The Airlock");
         Thread.sleep(1500);
+        System.out.println("> You notice that the locker that typically contains your space suit part is empty. ");
+        Thread.sleep(1500);
+        System.out.println("> 'I must have misplace my gear last night, I can't remember much about last night I guess'");
+        System.out.println("SELECT: Hall, Space");
+        
+        String choice = userInput.nextLine();
+        switch(choice){
+            case "hall":
+                Hall1();
+                break;
+            case "space":
+            //TODO: Once inventory tracking is implemented, check if spacesuit is fully assembled then determine ending
+            /*
+             * If fully assembled Endings.spaceDonut();
+             * If not fully assembled Endings.dead();
+             * 
+             * Defaulting to dead for now
+             */
+                Endings.dead(); //TEMP FOR NOW!!
+                break;
+            default:
+            System.out.println("Please make a valid choice (Case Sensitive, all lowercase)");
+            Airlock();
+            break;
+        }
 
         userInput.close();
     }
 
-    //Hall 1 (Connnected to Bedroom1, Bedroom2, Dining Room, Airlock, Hall2 and Hall3)
-    public static void hall1(){//TODO: Finish (Dustin)
-        System.out.println("Hall Test");
+    /*   Bedroom 1 (The Starting Room)
+     *   This room contains 7 exits
+     *   EXITS: Bedroom1, Bedroom2, Bedroom3, Dining Room, Hall2, Hall3, Airlock
+    */
+    public static void Hall1() throws InterruptedException{
+        Scanner userInput = new Scanner(System.in);
+        
+        System.out.println("\nLOCATION: Main Level");
+        Thread.sleep(1500);
+        System.out.println("> The main level hallway, this level contains many residential needs such as living and dining");
+        Thread.sleep(1500);
+        System.out.println("> This level also contains the airlock to go out into space and stairs to the upper and lower levels");
+        System.out.println("SELECT: Bedroom 1, Bedroom 2, Bedroom 3, Dining, Airlock, Upstairs, Downstairs");
+        String choice = userInput.nextLine();
+        switch(choice){
+            case "bedroom 1":
+                Bedroom1();
+                break;
+            case "bedroom 2":
+                //TODO: Connect to Bedroom2 once made
+                break;
+            case "bedroom 3":
+                //TODO: Connect to Bedroom3 once made
+                break;
+            case "dining":
+                //TODO: Connect to Dining once made
+                break;
+            case "airlock":
+                Airlock();
+                break;
+            case "upstairs":
+                //TODO: Connect to Hall2 once made
+                break;
+            case "downstairs": 
+                //TODO: Connect to Hall3 once made
+                break;
+            default:
+            System.out.println("Please make a valid choice (Case Sensitive, all lowercase)");
+            Hall1();
+            break;
+        }
+
+        userInput.close();
     }
  
 
@@ -74,13 +179,13 @@ public class Rooms {
     //TODO:  BED/BR3
     //TODO:  Dining/Kitchen
 
-    //TODO:  Hall2
+    //TODO:  Hall2 (Upper Level)
     //TODO: Comms Room
     //TODO: Armory
     //TODO: Pilots room
 
 
-    //TODO: Hall3
+    //TODO: Hall3 (Lower Level)
     //TODO: Engine Roon
     //TODO: Boiler Room
     
