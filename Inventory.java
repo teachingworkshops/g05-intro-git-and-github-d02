3dffa0ecff025cd5bf97c0cb290be41965723a30
Aarush-Kapoor <kapoora1@wit.edu>
2024-01-31 20:49:38 -0500

    Add Inventory File

---
+++ Inventory.java
@@ -0,0 +1,59 @@
+public class Inventory {
+    private boolean helmet = false;
+    private boolean jacket = false;
+    private boolean pants = false;
+    private boolean boots = false;
+    private boolean fakeKey = false;
+    private boolean realKey = false;
+
+    // Getter methods for each item
+    public boolean hasHelmet() {
+        return helmet;
+    }
+
+    public boolean hasJacket() {
+        return jacket;
+    }
+
+    public boolean hasPants() {
+        return pants;
+    }
+
+    public boolean hasBoots() {
+        return boots;
+    }
+
+    public boolean hasFakeKey() {
+        return fakeKey;
+    }
+
+    public boolean hasRealKey() {
+        return realKey;
+    }
+
+    // Method to update the status of each item
+    public void collectHelmet() {
+        helmet = true;
+    }
+
+    public void collectJacket() {
+        jacket = true;
+    }
+
+    public void collectPants() {
+        pants = true;
+    }
+
+    public void collectBoots() {
+        boots = true;
+    }
+
+    public void collectFakeKey() {
+        fakeKey = true;
+    }
+
+    public void collectRealKey() {
+        realKey = true;
+    }
+}
+
